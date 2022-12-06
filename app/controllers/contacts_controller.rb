8c2c9513b7fe64630c05640db8cc9e4cb83db43f
Juwon Do <nobeldr13@gmail.com>
2022-12-06 13:29:45 -0600

    add gem faker and code on db:seed

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,4 +3,9 @@ class ContactsController < ApplicationController
     contact1 = Contact.first
     render json: contact1.as_json
   end
+  def all_contacts
+    contact = Contact.all
+    render json: contact.as_json
+  end  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get "get_contacts" => "contacts#action"
+  # get "get_contacts" => "contacts#action"
+
+  get "get_contacts" => "contacts#all_contacts"
 end
